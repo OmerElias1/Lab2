89201ef38938a80ff2c04520c3f142faf131765f
Omer Kamil <Omereliaskamil@gmail.com>
2026-08-28 19:23:07 -0700

    feat: implement new stylized LoginForm component with background assets and react-icons support

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,37 +1,6 @@
-import { useState } from 'react'
-import HomePage from './component/homepage'
-import UserManagement from './usermangment'
-import './App.css'
-
+import LoginForm from './component/LoginForm/LoginForm'
 export default function App() {
-  const [registeredUsers, setRegisteredUsers] = useState([])
-  const [currentUser, setCurrentUser] = useState(null)
-
-  const handleRegister = (newUser) => {
-    setRegisteredUsers([...registeredUsers, newUser])
-  }
-
-  const handleLogin = (user) => {
-    setCurrentUser(user)
-  }
-
-  const handleLogout = () => {
-    setCurrentUser(null)
-  }
-
   return (
-    <>
-      {currentUser ? (
-        <HomePage user={currentUser} onLogout={handleLogout} />
-      ) : (
-        <div className="auth-page-wrapper">
-          <UserManagement
-            registeredUsers={registeredUsers}
-            onLogin={handleLogin}
-            onRegister={handleRegister}
-          />
-        </div>
-      )}
-    </>
+    <div><LoginForm /></div>
   )
 }
